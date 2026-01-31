e30254bf275497d7ecfe6835f7888a881f60cf9f
Mario Diaz Valencia <39930455+kaptha@users.noreply.github.com>
2026-01-30 18:57:46 -0600

    Update database.module.ts
    poner variables de railway en database module

--- sat-cat-api/src/database/database.module.ts
+++ sat-cat-api/src/database/database.module.ts
@@ -1,20 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres', // Asumiendo que usas PostgreSQL
-      host: 'localhost', // O la dirección de tu servidor de base de datos
-      port: 5432, // Puerto por defecto de PostgreSQL
-      username: 'postgres',
-      password: 'sx1180',
-      database: 'catalogos',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: false, // Solo para desarrollo, no uses en producción
-      autoLoadEntities: true,
-      logging: true, // Añade esto para ver los logs de TypeORM
-      logger: 'advanced-console', // Y esto para un logging más detallado
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('PG_HOST'),
+        port: +configService.get<number>('PG_PORT') || 5432,
+        username: configService.get('PG_USERNAME'),
+        password: configService.get('PG_PASSWORD'),
+        database: configService.get('PG_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: false,
+        autoLoadEntities: true,
+        logging: configService.get('NODE_ENV') !== 'production',
+        logger: 'advanced-console',
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
