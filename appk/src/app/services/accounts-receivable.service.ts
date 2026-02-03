ecf965591818a00f038829a7c128e6deb11cdf02
Mario Diaz Valencia <39930455+kaptha@users.noreply.github.com>
2026-02-03 09:55:53 -0600

    Update accounts-receivable.service.ts

--- appk/src/app/services/accounts-receivable.service.ts
+++ appk/src/app/services/accounts-receivable.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError, from } from 'rxjs';
 import { tap, catchError, switchMap, map } from 'rxjs/operators';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
 import { AuthResponse } from '../models/auth.model';
 
