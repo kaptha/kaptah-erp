4be828be0b39332d6147345b584e9e6ae099c01a
Mario Diaz Valencia <39930455+kaptha@users.noreply.github.com>
2026-02-03 09:52:23 -0600

    Update accounts-receivable.service.ts
    cambio de url

--- appk/src/app/services/accounts-receivable.service.ts
+++ appk/src/app/services/accounts-receivable.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError, from } from 'rxjs';
 import { tap, catchError, switchMap, map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 import { AuthService } from './auth.service';
 import { AuthResponse } from '../models/auth.model';
 
@@ -73,7 +73,7 @@ export interface PaymentData {
   providedIn: 'root'
 })
 export class AccountsReceivableService {
-  private apiUrl = 'http://localhost:3003/accounts-receivable';
+  private apiUrl = `${environment.financeApiUrl}/accounts-receivable`;
 
   constructor(
     private http: HttpClient,
