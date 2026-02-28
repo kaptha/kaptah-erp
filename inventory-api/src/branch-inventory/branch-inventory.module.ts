2130a8f3f87586cb2263019648b1c5e183c2bf98
Mario Diaz Valencia <39930455+kaptha@users.noreply.github.com>
2026-02-28 11:43:47 -0600

    Update branch-inventory.module.ts
    fix module

--- inventory-api/src/branch-inventory/branch-inventory.module.ts
+++ inventory-api/src/branch-inventory/branch-inventory.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BranchInventoryService } from './branch-inventory.service';
 import { BranchInventoryController } from './branch-inventory.controller';
 import { BranchInventory } from './entities/branch-inventory.entity';
-import { UsersModule } from '../users/users.module';
+import { UsersModule } from '../users/users.module'; 
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([BranchInventory], 'inventory'),
-    UsersModule
+    UsersModule 
   ],
   controllers: [BranchInventoryController],
   providers: [BranchInventoryService],
