3b5c98418830d110032053014ad76c13f26e3e32
Mario Diaz Valencia <39930455+kaptha@users.noreply.github.com>
2026-02-28 18:13:05 -0600

    Update branch-inventory.module.ts
    add authmodule

--- inventory-api/src/branch-inventory/branch-inventory.module.ts
+++ inventory-api/src/branch-inventory/branch-inventory.module.ts
@@ -4,11 +4,12 @@ import { BranchInventoryService } from './branch-inventory.service';
 import { BranchInventoryController } from './branch-inventory.controller';
 import { BranchInventory } from './entities/branch-inventory.entity';
 import { UsersModule } from '../users/users.module'; 
-
+import { AuthModule } from '../auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([BranchInventory], 'inventory'),
-    UsersModule 
+    UsersModule,
+    AuthModule
   ],
   controllers: [BranchInventoryController],
   providers: [BranchInventoryService],
