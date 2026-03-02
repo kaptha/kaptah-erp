b7d66e93e795e776120e2a7ebc8f9a81afae8f92
Mario Diaz Valencia <39930455+kaptha@users.noreply.github.com>
2026-03-02 09:18:19 -0600

    Update inventario.module.ts
    add transfer

--- appk/src/app/pages/inventario/inventario.module.ts
+++ appk/src/app/pages/inventario/inventario.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BranchInventoryListComponent } from './branch-inventory-list/branch-inventory-list.component';
+import { BranchTransfersListComponent } from './branch-transfers-list/branch-transfers-list.component';
+import { StockAlertsComponent } from './stock-alerts/stock-alerts.component';
 import { BranchInventoryService } from '../../services/inventory/branch-inventory.service';
 import { BranchTransferService } from '../../services/inventory/branch-transfer.service';
 
@@ -13,13 +15,22 @@ const routes: Routes = [
   {
     path: 'inventario-sucursales',
     component: BranchInventoryListComponent
+  },
+  {
+    path: 'transferencias',
+    component: BranchTransfersListComponent
+  },
+  {
+    path: 'alertas',
+    component: StockAlertsComponent
   }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
-    BranchInventoryListComponent
+    BranchInventoryListComponent,
+    BranchTransfersListComponent
   ],
   providers: [
     BranchInventoryService,
