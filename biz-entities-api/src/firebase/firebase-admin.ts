30b007606a27f846acd38c9dacc50010cdd0b130
Kaptah <quirumedik@gmail.com>
2026-04-02 13:42:32 -0600

    fix: add Firebase Database URL for Realtime DB access in createSubUser

--- biz-entities-api/src/firebase/firebase-admin.ts
+++ biz-entities-api/src/firebase/firebase-admin.ts
@@ -12,6 +12,7 @@ function initializeFirebaseAdmin() {
 
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
     });
   }
 }
